39c747d9d3d81bff0cc325b01fc0805eca6d87c6
李光春 <gc@dtapp.net>
2022-09-15 18:12:55 +0800

    - update

--- const.go
+++ const.go
@@ -3,5 +3,5 @@ package dingtalk
 const (
 	apiUrl   = "https://oapi.dingtalk.com"
 	logTable = "dingtalk"
-	Version  = "1.0.9"
+	Version  = "1.0.10"
 )
--- get.go
+++ get.go
@@ -1,9 +1,19 @@
 package dingtalk
 
+import "go.dtapp.net/golog"
+
 func (c *Client) GetSecret() string {
 	return c.config.secret
 }
 
 func (c *Client) GetAccessToken() string {
 	return c.config.accessToken
+}
+
+func (c *Client) GetLogGorm() *golog.ApiClient {
+	return c.log.logGormClient
+}
+
+func (c *Client) GetLogMongo() *golog.ApiClient {
+	return c.log.logMongoClient
 }
